361614fcba0ca62167c01eb72c55007cbc562e1d
chamorcev <stud0000299683@utmn.ru>
2025-09-15 17:53:40 +0300

    first commit project 21

---
+++ src/main/java/ru/utmn/chamortsev/netflix/controller/NetflixController.java
@@ -0,0 +1,12 @@
+package ru.utmn.chamortsev.netflix.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NetflixController {
+@GetMapping("/hello")
+public String hello(){
+    return "Hello Word!";
+}
+}
